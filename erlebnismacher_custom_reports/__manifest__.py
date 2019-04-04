341c0ec66409113546098ed0f14cef935353b99e
VervaeckeC <45397642+VervaeckeC@users.noreply.github.com>
2019-04-04 18:08:04 +0200

    test

---
+++ erlebnismacher_custom_reports/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'erlebnismacher_custom_reports',
+    'description': """Custom Reports for Erlebnismacher""",
+    'depends': [
+        'sale', 'account',
+    ],
+    'data': [
+        'templates/templates.xml',
+    ],
+}
